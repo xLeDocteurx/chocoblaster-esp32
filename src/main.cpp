e45280cfc6df5353d13dfd78352b72d41d20553d
clenoir <christopher.lenoir@webinage.fr>
2024-02-06 15:29:22 +0100

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <Arduino.h>
 
 #include "hidkeyboard.h"
-#include "Wire.h"
+// #include "Wire.h"
 
 #if CFG_TUD_HID
 #define KEYBOARD_I2C_ADDR 0X5f
-// #endif
 
-HIDkeyboard dev;
+HIDkeyboard keyboard;
 const int buttonPin = 4;        // input pin for pushbutton
 const int ledPin = 2;           // input pin for pushbutton
 int previousButtonState = HIGH; // for checking the state of a pushButton
@@ -16,15 +15,21 @@ int counter = 0;                // button push counter
 void setup()
 {
   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  // pinMode(buttonPin, INPUT_PULLDOWN);
 
-  digitalWrite(ledPin, !HIGH);
+  digitalWrite(ledPin, HIGH);
+
+  keyboard.begin();
+  // device.manufacturer(char*);
+  // device.product(char*); // product name
+  // device.serial(char*);  // serial number SN
+  // device.revision(uint16_t); // product revison
+  // device.deviceID(uint16_t VID, uint16_t PID);
+  // device.deviceID(uint16_t* VID, uint16_t* PID);
+
   vTaskDelay(pdMS_TO_TICKS(1000));
-  pinMode(buttonPin, INPUT_PULLDOWN);
-
-  Wire.begin(7, 8);
-  dev.begin();
-
-  digitalWrite(ledPin, !LOW);
+  digitalWrite(ledPin, LOW);
   printf("INIT DONE\n");
 }
 
@@ -32,57 +37,41 @@ void loop()
 {
   // read the pushbutton:
   int buttonState = digitalRead(buttonPin);
+  // printf("buttonState : %i\n", buttonState);
   // if the button state has changed,
   if ((buttonState != previousButtonState)
-      // and it's currently pressed:
+      // and is currently pressed:
       && (buttonState == HIGH))
   {
     printf("BUTTON PRESSED\n");
-    // // increment the button counter
-    // counter++;
-    // // type out a message
-    // Keyboard.print("You pressed the button ");
-    // Keyboard.print(counter);
-    // Keyboard.println(" times.");
 
-    // Keyboard.write(KEY_HOME); // KEY_MENU ? // KEY_LEFT_GUI ?
+    // Serial.println(dev.sendString(String("123456789\n"))?"OK":"FAIL");
+
+    // keyboard.sendKey(HID_KEY_HOME); // KEY_MENU ? // KEY_LEFT_GUI ?
     // delay(100);
-    // Keyboard.println("slack");
+    // keyboard.sendString("slack");
     // delay(100);
-    // Keyboard.press(KEY_LEFT_CTRL);
+    // keyboard.sendKey(HID_KEY_ENTER);
     // delay(100);
-    // Keyboard.press("f");
+    // keyboard.sendPress(HID_KEY_CONTROL_LEFT);
     // delay(100);
-    // Keyboard.releaseAll();
+    // keyboard.sendPress(HID_KEY_F);
     // delay(100);
-    // Keyboard.print("#general");
+    // keyboard.sendRelease();
     // delay(100);
-    // Keyboard.write(KEY_UP_ARROW);
+    // keyboard.sendString("#general");
     // delay(100);
-    // Keyboard.write(KEY_RETURN); // KEY_KP_ENTER ? // Keyboard.println(); ?
+    // keyboard.sendKey(HID_KEY_ARROW_UP);
     // delay(100);
-    // Keyboard.println("Demain je paye ma tournée de pains au chocolat !\nParce que l’on dit pain au chocolat.\n\nCeci est un chocoblast . Pour votre sécurité n’oubliez pas de verrouiller vos postes et de ne pas laisser Christopher brancher un périphérique usb douteux dessus.");
+    // keyboard.sendKey(HID_KEY_ENTER);
+    // delay(100);
+    keyboard.sendString("Demain je paye ma tournée de pains au chocolat !\nParce que l’on dit pain au chocolat.\n\nCeci est un chocoblast . Pour votre sécurité n’oubliez pas de verrouiller vos postes et de ne pas laisser Christopher brancher un périphérique usb douteux dessus.");
+    // delay(100);
+    // keyboard.sendKey(HID_KEY_ENTER);
+
+    // previousButtonState = buttonState;
   }
-
-  // // save the current button state for comparison next time:
-  // previousButtonState = buttonState;
-
-  // delay(10);
-  // Wire.requestFrom(KEYBOARD_I2C_ADDR, 1); // request 1 byte from keyboard
-  // while (Wire.available())
-  // {
-  //   uint8_t key_val = Wire.read(); // receive a byte as character
-  //   if (key_val != 0)
-  //   {
-  //     dev.sendChar(key_val); // send ASCII char
-  //   }
-  // }
-}
-
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+    previousButtonState = buttonState;
 }
 
 #endif
